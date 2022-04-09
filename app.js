9ba1499d3f4192a8a9688e7db14cb303feed6807
Jan Michalcak <jan.michalcak@gmail.com>
2022-04-10 01:24:05 +0200

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const timeLeftDisplay = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#result");
+const button = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
+
+function moveFrog() {
+  console.log("moved");
+}
+
+document.addEventListener("keyup", moveFrog);
