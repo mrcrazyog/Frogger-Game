9294815fcacb750cb4833a05fa5ec91d62a77305
Jan Michalcak <jan.michalcak@gmail.com>
2022-04-11 20:57:03 +0200

    added frog

--- app.js
+++ app.js
@@ -2,9 +2,31 @@ const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const button = document.querySelector("#start-pause-button");
 const squares = document.querySelectorAll(".grid div");
+let currentIndex = 76;
+const width = 9;
 
-function moveFrog() {
-  console.log("moved");
+function moveFrog(e) {
+  squares[currentIndex].classList.remove("frog");
+
+  switch (e.key) {
+    case "ArrowLeft":
+      console.log("Move left");
+      if (currentIndex % width !== 0) currentIndex -= 1;
+      break;
+    case "ArrowRight":
+      console.log("Move Right");
+      if (currentIndex % width < width - 1) currentIndex += 1;
+      break;
+    case "ArrowUp":
+      console.log("Move Up");
+      if (currentIndex - width >= 0) currentIndex -= width;
+      break;
+    case "ArrowDown":
+      console.log("Move Down");
+      if (currentIndex + width < width * width) currentIndex += width;
+      break;
+  }
+  squares[currentIndex].classList.add("frog");
 }
 
 document.addEventListener("keyup", moveFrog);
